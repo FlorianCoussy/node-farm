9c93305360a3770f6a7f7b8d55e3222b263cb63f
Florian Coussy <florian.coussy@gmail.com>
2024-11-21 16:11:27 -0500

    Add generateView method and Update products view

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 const fs = require("fs");
 const http = require("http");
-const url = require("url");
 
 const PORT = 8000;
 
 const products = fs.readFileSync(`${__dirname}/models/data.json`, "UTF-8");
+const productsObj = JSON.parse(products);
+
 const overviewView = fs.readFileSync(`${__dirname}/views/overview.html`, "UTF-8");
-const productView = fs.readFileSync(`${__dirname}/views/product-page.html`, "UTF-8");
+const productPageView = fs.readFileSync(`${__dirname}/views/product-page.html`, "UTF-8");
+const productRecordView = fs.readFileSync(`${__dirname}/views/product-record.html`, "UTF-8");
+
+const generateView = (template, placeholders, data) => {
+  return data.map((product) => {
+    let recordView = template;
+    for ([placeholder] of placeholders) {
+      const placeholderKey = placeholder.substring(1, placeholder.length-1);
+      recordView = recordView.replace(placeholder, product[placeholderKey]);
+    }
+    return recordView;
+  });
+}
 
 const server = http.createServer((req, res) => {
   const { url: path } = req;
 
   if (path === "/" || path === "/overview") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(overviewView);
+    
+    const placeholders = Array.from(productRecordView.matchAll(/{\w*}/g));
+    const productsView = generateView(productRecordView, placeholders, productsObj);
+    
+    res.end(overviewView.replace("{products}", productsView));
   } else if (path === "/product") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(productView);
+    res.end(productPageView);
   } else if (path === "/api") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(products);
+    res.end(productsObj);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1>404 Page not found!</h1>");
